3c53c64ea6b471ce1bab64d9d483b556ce7aea82
Juan Luis García Borrego <juanluisgarciaborrego@emergya.com>
2016-04-22 10:53:32 +0200

    datafixturs user admin

---
+++ src/AppBundle/DataFixtures/ORM/LoadUserData.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace AppBundle\DataFixtures\ORM;
+
+use Doctrine\Common\DataFixtures\AbstractFixture;
+use Doctrine\Common\DataFixtures\OrderedFixtureInterface;
+use Doctrine\Common\Persistence\ObjectManager;
+use Symfony\Component\DependencyInjection\ContainerAwareInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+class LoadUserData extends AbstractFixture implements OrderedFixtureInterface, ContainerAwareInterface
+{
+    /**
+     * @var ContainerInterface
+     */
+    private $container;
+    /**
+     * {@inheritDoc}
+     */
+    public function setContainer(ContainerInterface $container = null)
+    {
+        $this->container = $container;
+    }
+    /**
+     * {@inheritDoc}
+     */
+    public function load(ObjectManager $manager)
+    {
+        $userManager = $this->container->get('fos_user.user_manager');
+
+        $user = $userManager->createUser();
+        $user->setUsername('admin');
+        $user->setEmail('admin@symfonyzero.es');
+        $user->setPlainPassword('admin');
+        $user->setRoles(['ROLE_ADMIN']);
+        $user->setEnabled(true);
+
+        $userManager->updateUser($user);
+    }
+
+    /**
+     * Get the order of this fixture
+     *
+     * @return integer
+     */
+    function getOrder()
+    {
+        return 1;
+    }
+}
