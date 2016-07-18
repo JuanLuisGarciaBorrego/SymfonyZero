36843307cc6e694a795dcc4d89c0262fb84c00ae
Alberto Beiztegui Casado <albertobeiz@gmail.com>
2016-07-18 19:49:25 +0200

    Added composer commands

--- src/AppBundle/Command/DeployCommand.php
+++ src/AppBundle/Command/DeployCommand.php
@@ -9,6 +9,8 @@ use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Input\ArrayInput;
 use Symfony\Component\Console\Output\BufferedOutput;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Process\Process;
+use Symfony\Component\Process\Exception\ProcessFailedException;
 
 
 class DeployCommand extends Command
@@ -25,6 +27,10 @@ class DeployCommand extends Command
     protected function execute(InputInterface $input, OutputInterface $output)
     {
       $this->output = $output;
+
+      $this->runProccess('composer install');
+      $this->runProccess('composer update');
+
       $this->runCommand('doctrine:schema:update', array('--force' => true));
 
       $this->runCommand('assetic:dump', array('--env' => 'prod', '--no-debug' => true));
@@ -33,14 +39,30 @@ class DeployCommand extends Command
       $this->runCommand('cache:clear', array('--env' => 'prod'));
     }
 
+    protected function runProccess($command)
+    {
+      $this->output->writeln('<comment>Running command: '.$command.'<comment>');
+      $process = new Process($command);
+      $process->run();
+
+      // executes after the command finishes
+      if (!$process->isSuccessful()) {
+          throw new ProcessFailedException($process);
+      }
+
+      $this->output->write($process->getOutput());
+      $this->output->writeln('<info>Finished command: '.$command.'<info>');
+      $this->output->writeln('');
+    }
+
     protected function runCommand($command, $arguments)
     {
-      $this->output->writeln('Running command: '.$command);
+      $this->output->writeln('<comment>Running command: '.$command.'<comment>');
       $applicationCommand = $this->getApplication()->find($command);
       $input = new ArrayInput($arguments);
 
       $applicationCommand->run($input, $this->output);
-      $this->output->writeln('Finished command: '.$command);
+      $this->output->writeln('<info>Finished command: '.$command.'<info>');
       $this->output->writeln('');
     }
 }
