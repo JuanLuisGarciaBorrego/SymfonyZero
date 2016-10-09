6beade195b7a2c69e25e1505f44e2e1f6ea090cc
rcarballoc <rcarballoc@emergya.com>
2016-10-09 10:54:25 +0200

    Script fixes.

--- deploy/setup.sh
+++ deploy/setup.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
+
+if ! type apt-get > /dev/null 2>&1; then
+    echo "This script only runs on an apt-get package manager available system"
+    exit
+fi
+
+
 RED='\033[0;31m'
 GREEN='\033[0;32m'
 YELLOW='\033[0;33m'
@@ -6,7 +13,6 @@ NC='\033[0m' # No Color
 
 SYMFONYPATH='/var/www/SymfonyZero'
 BRANCH='master'
-
 
 # Apache
 if ! type apache2 > /dev/null 2>&1; then
@@ -50,40 +56,54 @@ if ! type composer > /dev/null 2>&1; then
     sudo mv composer.phar /usr/local/bin/composer
 fi
 
-# Downloads SymfonyZero web folder
-if [ ! -d "$SYMFONYPATH" ]; then
-  git clone https://github.com/Emergya/SymfonyZero $SYMFONYPATH
-fi
-# Update repo and Symfony deploy
-cd $SYMFONYPATH
-printf "${GREEN}Installing vendors:${NC} "
-composer install
+# Downloads/Update SymfonyZero 
+GIT_ACTION="clone https://github.com/rcarballoc/SymfonyZero $SYMFONYPATH"
+COMPOSER_ACTION="install"
 
-php bin/console doctrine:database:create
+if [ -d "$SYMFONYPATH" ]; then
+  cd $SYMFONYPATH
+  GIT_ACTION="pull origin"  
+  COMPOSER_ACTION="update"  
+fi
+
+# Update repo and Symfony deploy
+git $GIT_ACTION
+
+cd $SYMFONYPATH
+
+printf "${GREEN}Install/Updating vendors:${NC} "
+composer $COMPOSER_ACTION
+
+#Create database only if we're on installing 
+if [ "$COMPOSER_ACTION" = "install" ]; then
+    php bin/console doctrine:database:create
+else
+   printf "${YELLOW}Skipping Database creation${NC} "    
+fi
+
 php bin/console doctrine:schema:update --force
 php bin/console doctrine:fixtures:load
 php bin/console assetic:dump --env=prod --no-debug
 php bin/console cache:clear
-sudo chmod -R 777 var/cache/
-sud ochmod -R 777 var/logs/
+sudo chmod -R 777 $SYMFONYPATH/var/cache/
+sudo chmod -R 777 $SYMFONYPATH/var/logs/
 
 
 
-printf "${GREEN}Configuring Apache and restarting:${NC} "
+printf "${GREEN}Configuring Apache Virtualhost and restarting:${NC} "
 # Update and enable Apache2 config
-#rm /etc/apache2/sites-enabled/000-default.conf
-#rm /etc/apache2/sites-available/000-default.conf 
 cp $SYMFONYPATH/deploy/symfonyzero.conf /etc/apache2/sites-available/symfonyzero.conf 
-
 ln -s /etc/apache2/sites-available/symfonyzero.conf /etc/apache2/sites-enabled
 service apache2 restart
 
 #Add entry in /etc/hosts
 printf "\n127.0.0.1\tsymfonyzero\n" >> /etc/hosts
 
-
-
-
-
-
-
+#EOP
+printf "\n${GREEN}Install completed."
+printf "\n--------------------------"
+printf "\nIMPORTANT: Developer environment is default enabled so if your"
+printf "\ndeployment has been in an LXC container or an external server you "
+printf "\nhave to configure the proper values for Production or enable remote "
+printf "\naccess to your IP in the app_dev.php file\n\${NC}".
+printf "\n${GREEN}For further information, please refer to http://www.symfony.com\n\n${NC}".
