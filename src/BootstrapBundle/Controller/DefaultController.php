8e491da8160e9a13eab33fdb30169df570c5699d
Maxfire <beisbol_2@hotmail.com>
2016-10-23 14:09:23 +0200

    db from carruselbundle added

--- src/BootstrapBundle/Controller/DefaultController.php
+++ src/BootstrapBundle/Controller/DefaultController.php
@@ -5,15 +5,31 @@ namespace BootstrapBundle\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use AppBundle\Entity\Carrusel;
 
 class DefaultController extends Controller
 {
     /**
-     * @Route("/carrusel/{name}")
+     * @Route("/carrusel")
      * @Template()
+     *
      */
-    public function indexAction($name)
+    public function indexAction()
     {
-        return array('name'=>$name);
+     $text1 = $this->getDoctrine()
+         ->getRepository('AppBundle:Carrusel')
+         ->find(1);
+     $text2= $this->getDoctrine()
+         ->getRepository('AppBundle:Carrusel')
+         ->find(2);
+     $text3 = $this->getDoctrine()
+         ->getRepository('AppBundle:Carrusel')
+         ->find(3);
+     $text4 = $this->getDoctrine()
+         ->getRepository('AppBundle:Carrusel')
+         ->find(4);
+
+        return $this->render('BootstrapBundle:Default:index.html.twig', array('text1' =>$text1->getText(),
+            'text2' =>$text2->getText(),'text3' =>$text3->getText(),'text4' =>$text4->getText()));
     }
 }
